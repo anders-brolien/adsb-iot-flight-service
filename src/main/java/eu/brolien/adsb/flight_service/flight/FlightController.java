66636b246e22de5080b790db8dfbb23c2441d12d
andersbrolien <andersbrolien@10.0.1.22>
2016-08-18 19:48:19 +0200

    multi device support

--- src/main/java/eu/brolien/adsb/flight_service/flight/FlightController.java
+++ src/main/java/eu/brolien/adsb/flight_service/flight/FlightController.java
@@ -1,5 +1,6 @@
 package eu.brolien.adsb.flight_service.flight;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -62,75 +63,89 @@ public class FlightController {
 	}
 
 	private void convertFlights(FeatureCollection result, List<Map<String, AttributeValue>> flights) {
+		Map<String, Feature> features = new HashMap<>();
 		for (Map<String, AttributeValue> item : flights) {			
 			Map<String, AttributeValue> payload = item.get("payload").getM();			
 			if (payload.get("validposition").getN().equals("1")) {				
-				double lat = Double.parseDouble(payload.get("lat").getN());
-				double lon = Double.parseDouble(payload.get("lon").getN());
-				String cs = payload.get("flight").getS();
-				int alt = Integer.parseInt(payload.get("altitude").getN());
-				long timestamp = Long.parseLong(payload.get("timestamp").getN());
-				long speed = Long.parseLong(payload.get("speed").getN());
-				long track = Long.parseLong(payload.get("track").getN());
-
-				String hex = payload.get("hex").getS();
-				Feature f = new Feature();
-				
-				f.setGeometry(new Point(new LngLatAlt(lon, lat)));
-				f.setId(hex);
-				f.setProperty("altitude", alt);
-				f.setProperty("callsign", cs);
-				f.setProperty("timestamp", timestamp);
-				f.setProperty("speed", speed);
-				
-				String SIDC = "SFAPMF------";
-				if (System.currentTimeMillis() - timestamp > 30 * 1000) {
-					SIDC = "SFAAMF------";
-				}
-				
-				f.setProperty("SIDC", SIDC);
-
-				if (payload.get("validtrack").getN().equals("1")) {			
-					f.setProperty("bearing", track);
-				}
-				result.add(f);
-				
+				Feature f = extractFlightFeature(payload);				
+				if (features.containsKey(f.getId())) {					
+					Feature current = features.get(f.getId());
+					current.setProperty("device", f.getProperty("device") + " " + current.getProperty("device"));
+				} else {
+					result.add(f);									
+				}				
 			}
 			
 		}
 	}
+
+	private Feature extractFlightFeature(Map<String, AttributeValue> payload) {
+		double lat = Double.parseDouble(payload.get("lat").getN());
+		double lon = Double.parseDouble(payload.get("lon").getN());
+		String cs = payload.get("flight").getS();
+		int alt = Integer.parseInt(payload.get("altitude").getN());
+		long timestamp = Long.parseLong(payload.get("timestamp").getN());
+		long speed = Long.parseLong(payload.get("speed").getN());
+		long track = Long.parseLong(payload.get("track").getN());
+		String hex = payload.get("hex").getS();
+		Feature f = new Feature();
+		
+		f.setGeometry(new Point(new LngLatAlt(lon, lat)));
+		f.setId(hex);
+		f.setProperty("altitude", alt);
+		f.setProperty("callsign", cs);
+		f.setProperty("timestamp", timestamp);
+		f.setProperty("speed", speed);
+		f.setProperty("device", payload.get("device").getS());
+		
+		String SIDC = "SFAPMF------";
+		if (System.currentTimeMillis() - timestamp > 30 * 1000) {
+			SIDC = "SFAAMF------";
+		}
+		
+		f.setProperty("SIDC", SIDC);
+
+		if (payload.get("validtrack").getN().equals("1")) {			
+			f.setProperty("bearing", track);
+		}
+		return f;
+	}
 	
 	
 	private void convertDevices(FeatureCollection result, List<Map<String, AttributeValue>> flights) {
+		Map<String, Feature> map = new HashMap<>();
+		
 		for (Map<String, AttributeValue> item : flights) {			
 			Map<String, AttributeValue> payload = item.get("payload").getM();			
-				double lat = Double.parseDouble(payload.get("lat").getN());
-				double lon = Double.parseDouble(payload.get("lon").getN());
-				String device = payload.get("device").getS();
-				long timestamp = Long.parseLong(payload.get("timestamp").getN());
-				long startup = Long.parseLong(payload.get("startup").getN());
-				long messages = Long.parseLong(payload.get("messages").getN());
-
-				Feature f = new Feature();
-				
-				f.setGeometry(new Point(new LngLatAlt(lon, lat)));
-				f.setId(device);
-				f.setProperty("device", device);
-				f.setProperty("timestamp", timestamp);
-				f.setProperty("startup", startup);
-				f.setProperty("messages", messages);
-				
-				String SIDC = "SFGPESR-----";
-				if (System.currentTimeMillis() - timestamp > 5 * 60 * 1000) {
-					SIDC = "SFGAESR-----";
-				}
-				
-				f.setProperty("SIDC", SIDC);
-				
+				Feature f = extractDeviceFeature(payload);
 				result.add(f);
-				
-			
 		}
+	}
+
+	private Feature extractDeviceFeature(Map<String, AttributeValue> payload) {
+		double lat = Double.parseDouble(payload.get("lat").getN());
+		double lon = Double.parseDouble(payload.get("lon").getN());
+		String device = payload.get("device").getS();
+		long timestamp = Long.parseLong(payload.get("timestamp").getN());
+		long startup = Long.parseLong(payload.get("startup").getN());
+		long messages = Long.parseLong(payload.get("messages").getN());
+
+		Feature f = new Feature();
+		
+		f.setGeometry(new Point(new LngLatAlt(lon, lat)));
+		f.setId(device);
+		f.setProperty("device", device);
+		f.setProperty("timestamp", timestamp);
+		f.setProperty("startup", startup);
+		f.setProperty("messages", messages);
+		
+		String SIDC = "SFGPESR-----";
+		if (System.currentTimeMillis() - timestamp > 5 * 60 * 1000) {
+			SIDC = "SFGAESR-----";
+		}
+		
+		f.setProperty("SIDC", SIDC);
+		return f;
 	}
 	
 
